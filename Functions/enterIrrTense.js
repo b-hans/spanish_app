a5eae616e8385570164ff46f945afc1570888bf8
Brian Hanson <brianhanson@Brians-MacBook-Pro.local>
2026-08-05 15:00:18 -0500

    subjunctive enter done

--- Functions/enterIrrTense.js
+++ Functions/enterIrrTense.js
@@ -9,28 +9,17 @@ function enterIrrTense (CURRENT_TYPE) {
         let topHeaders;
         let sideHeaders = PRONOUN_RANGE_ARRAY;
 
-        console.log (sideHeaders);
-
-        let filteredConjugationTypes;
-
         switch (CURRENT_TYPE.tense) {
             case "Subjunctive":
 
                 let conjugationData = CONJUGATION_TYPES.getDataRange().getValues();
                 let conjugationHeaders = conjugationData.shift();
 
-                filteredConjugationTypes = conjugationData.filter (
-                    row => row[conjugationHeaders.indexOf('conjugation type')].
-                    startsWith ("Subjunctive ")
-                );
-
-                console.log (filteredConjugationTypes);
-
                 topHeaders = [
                     { 
                         tense:  'Subjunctive present',
                         column: 0,
-                        id:     filteredConjugationTypes.filter(
+                        id:     conjugationData.filter(
                                     row => row[conjugationHeaders
                                         .indexOf('conjugation type')] == "Subjunctive present"
                                 )[0][conjugationHeaders.indexOf('conjugation_type_id')],
@@ -38,30 +27,58 @@ function enterIrrTense (CURRENT_TYPE) {
                     {
                         tense:  'Subjunctive imperfect',
                         column: 1,
-                        id:     filteredConjugationTypes.filter(
+                        id:     conjugationData.filter(
                                     row => row[conjugationHeaders
                                         .indexOf('conjugation type')] == "Subjunctive imperfect"
                                 )[0][conjugationHeaders.indexOf('conjugation_type_id')],
                     },
                     {
                         tense:  'Subjunctive future',
-                        column: 2,
-                        id:     filteredConjugationTypes.filter(
+                        column: 3,
+                        id:     conjugationData.filter(
                                     row => row[conjugationHeaders
                                         .indexOf('conjugation type')] == "Subjunctive future"
                                 )[0][conjugationHeaders.indexOf('conjugation_type_id')],
                     }
                 ];
 
-                console.log (topHeaders);
+                let newData = new Array();
 
+                // create a row for each tense and push it onto the newData
+                for (let i=0; i<topHeaders.length; i++) {
+                    let item = topHeaders[i];
+                    let row = [];
+
+                    row.push(CURRENT_TYPE.verb_id);
+                    row.push(item.id);
+
+                    for (let j=0; j<sideHeaders.length; j++) {
+                        row.push(CURRENT_TYPE.data[j][item.column])
+                    }
+
+                    newData.push(row);
+
+                }
+
+                IRREGULAR_TENSES.getRange(
+                    IRREGULAR_TENSES.getLastRow() + 1,
+                    1,
+                    newData.length,
+                    newData[0].length
+                ).setValues(newData);
                 break;
         }
 
-        // get the data and build rows
-        console.log (CURRENT_TYPE);
+        CURRENT_TYPE.status = "read_verb";
+        CURRENT_TYPE.data = null;
+        CURRENT_TYPE.tense = null;
+        CURRENT_TYPE.tenseEmpty = false;
 
-        display.setValue ("checkem");
+        CACHE.put('CURRENT_TYPE', JSON.stringify(CURRENT_TYPE), 3600);
+
+        verbOut();
+
+        display.setValue ("Tense added!");
         return true;
     }
     catch (error) {
